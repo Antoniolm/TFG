ecec13ed9a15dc7c46ebe99eb3ebd7706e47d911
Antoniolm <codiak19@gmail.com>
2016-11-07 14:37:57 +0100

    Added a new class -> Material
    We can create material for our mesh

---
+++ SDLGame-TFG/GraphicEngine/material.cpp
@@ -0,0 +1,98 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#include "material.h"
+
+Material::Material()
+{
+    //ctor
+}
+
+//**********************************************************************//
+
+Material::Material(const Material & aMaterial){
+    ambient=aMaterial.ambient;
+    diffuse=aMaterial.diffuse;
+    specular=aMaterial.specular;
+    shininess=aMaterial.shininess;
+}
+
+//**********************************************************************//
+
+Material::Material(const vec3f & anAmbient,const vec3f & aDiffuse,const vec3f &aSpecular,float aShini){
+    ambient=anAmbient;
+    diffuse=aDiffuse;
+    specular=aSpecular;
+    shininess=aShini;
+}
+
+//**********************************************************************//
+
+Material::~Material()
+{
+    //dtor
+}
+
+//**********************************************************************//
+
+void Material::setAmbient(const vec3f & anAmbient){
+    ambient=anAmbient;
+}
+
+//**********************************************************************//
+
+void Material::setDiffuse(const vec3f & aDiffuse){
+    diffuse=aDiffuse;
+}
+
+//**********************************************************************//
+
+void Material::setSpecular(const vec3f &aSpecular){
+    specular=aSpecular;
+}
+
+//**********************************************************************//
+
+float Material::setShininess(float aShini){
+    shininess=aShini;
+}
+
+//**********************************************************************//
+
+vec3f Material::getAmbient(){
+    return ambient;
+}
+
+//**********************************************************************//
+
+vec3f Material::getDiffuse(){
+    return diffuse;
+}
+
+//**********************************************************************//
+
+vec3f Material::getSpecular(){
+    return specular;
+}
+
+//**********************************************************************//
+
+float Material::getShininess(){
+    return shininess;
+}
---
+++ SDLGame-TFG/GraphicEngine/material.h
@@ -0,0 +1,68 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "structdata.h"
+
+class Material
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        Material();
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Copy constructor */
+        //////////////////////////////////////////////////////////////////////////
+        Material(const Material & aMaterial);
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor with parameters*/
+        //////////////////////////////////////////////////////////////////////////
+        Material(const vec3f & anAmbient,const vec3f & aDiffuse,const vec3f &aSpecular,float aShini);
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~Material();
+
+        void setAmbient(const vec3f & anAmbient);
+        void setDiffuse(const vec3f & aDiffuse);
+        void setSpecular(const vec3f &aSpecular);
+        float setShininess(float aShini);
+
+
+        vec3f getAmbient();
+        vec3f getDiffuse();
+        vec3f getSpecular();
+        float getShininess();
+
+    protected:
+
+    private:
+        vec3f ambient;
+        vec3f diffuse;
+        vec3f specular;
+        float shininess;
+};
+
+#endif // MATERIAL_H
