4ef0f13d07793fdcf3b7eaba2d3ad52ac86ab43a
Antoniolm <codiak19@gmail.com>
2016-12-04 17:47:15 +0100

    Added a new class -> IANpc
    That class will manage the conversation with the npcs

---
+++ SDLGame-TFG/GraphicEngine/ianpc.h
@@ -0,0 +1,60 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef IANPC_H
+#define IANPC_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class IANpc
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        IANpc();
+
+        IANpc(const IANpc & aIA);
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~IANpc();
+
+        void addState(const string & state);
+        void removeState(int id);
+        string & getCurrentState();
+        int getNumState();
+        void clean();
+        void nextState();
+        void beforeState();
+        bool isLastState();
+
+    protected:
+
+    private:
+        vector<string>  states;
+        int currentState;
+
+};
+
+#endif // IANPC_H
