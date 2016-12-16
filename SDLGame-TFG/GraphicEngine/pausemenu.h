c43e0a5cc161d466b30f0480fa5cbf235256aa6e
Antoniolm <codiak19@gmail.com>
2016-12-16 11:55:53 +0100

    Added a new class PauseMenu : public Menu
    That menu will be use when the user pause the menu. I need to load all
    the information of menu to pause menu because i dont have only one type
    of menu .

---
+++ SDLGame-TFG/GraphicEngine/pausemenu.h
@@ -0,0 +1,38 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef PAUSEMENU_H
+#define PAUSEMENU_H
+
+#include "menu.h"
+
+class PauseMenu : public Menu
+{
+    public:
+        /** Default constructor */
+        PauseMenu();
+        /** Default destructor */
+        virtual ~PauseMenu();
+
+    protected:
+
+    private:
+};
+
+#endif // PAUSEMENU_H
