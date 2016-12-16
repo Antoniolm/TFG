c50b5cfcbc85cd391b3259e07e9ce162d908cb01
Antoniolm <codiak19@gmail.com>
2016-12-16 12:19:14 +0100

    Added a new class -> MainMenu
    That type of menu will be the first menu in the game and that menu show
    you 4 options : Continue,Start Game, Controls,Quit

---
+++ SDLGame-TFG/GraphicEngine/mainmenu.h
@@ -0,0 +1,68 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include <object3d.h>
+#include "rootmap.h"
+#include "context.h"
+#include "mesh.h"
+#include "nodescenegraph.h"
+#include "texture.h"
+#include "material.h"
+#include "sound.h"
+#include "menu.h"
+
+class MainMenu : public Menu
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        MainMenu();
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~MainMenu();
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will show the object in our interface
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        virtual void visualization(Context & cv);
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will update the state of the object. That change need the
+        *    current time in our application
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        virtual void updateState(float time,const Uint8* currentKeyStates,RootMap * rootMap);
+
+    protected:
+
+    private:
+};
+
+#endif // MAINMENU_H
