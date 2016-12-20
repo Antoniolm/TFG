f389f00761cb23b4b3605772bf7bca446ccad74d
Antoniolm <codiak19@gmail.com>
2016-12-20 15:50:24 +0100

    Added a new class -> particlesystem
    The particlesystem will manage all the particle in one system for
    example a particlesystem can see a fire in the scene.

---
+++ SDLGame-TFG/GraphicEngine/particlesystem.h
@@ -0,0 +1,64 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef PARTICLESYSTEM_H
+#define PARTICLESYSTEM_H
+
+#include <object3d.h>
+#include <context.h>
+#include <rootmap.h>
+#include <vector>
+
+class ParticleSystem : public Object3D
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        ParticleSystem();
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~ParticleSystem();
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will show the object in our interface
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        void visualization(Context & cv);
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will update the state of the object. That change need the
+        *    current time in our application
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        void updateState(float time,const Uint8* currentKeyStates,RootMap * rootMap);
+
+    protected:
+
+    private:
+        vector<Particle *> particles;
+};
+
+#endif // PARTICLESYSTEM_H
