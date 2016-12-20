aeaa0655c2ebdd50827a9468a1b892c2aa215194
Antoniolm <codiak19@gmail.com>
2016-12-20 16:10:41 +0100

    Added another new class -> particle
    That class will show a unique particle and that particle is manage for
    one particleSystem

---
+++ SDLGame-TFG/GraphicEngine/particle.h
@@ -0,0 +1,63 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include "object3d.h"
+#include <context.h>
+#include <rootmap.h>
+
+class Particle : public Object3D
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        Particle();
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~Particle();
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will show the object in our interface
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        void visualization(Context & cv);
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will update the state of the object. That change need the
+        *    current time in our application
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        void updateState(float time,const Uint8* currentKeyStates,RootMap * rootMap);
+
+    protected:
+
+    private:
+        NodeSceneGraph *root;
+};
+
+#endif // PARTICLE_H
