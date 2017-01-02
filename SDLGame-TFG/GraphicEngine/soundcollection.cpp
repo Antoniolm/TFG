82241dde43d64dc475d5c28991f06febbc753026
Antoniolm <codiak19@gmail.com>
2017-01-02 13:31:28 +0100

    Added a new collection -> soundCollection

---
+++ SDLGame-TFG/GraphicEngine/soundcollection.cpp
@@ -0,0 +1,52 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#include "soundcollection.h"
+
+SoundCollection* SoundCollection::instance = NULL;
+
+//**********************************************************************//
+
+SoundCollection::SoundCollection()
+{
+    Sound * sound=new Sound("sounds/coin.wav",1,30,4,0);
+    collection.push_back(sound);
+
+    //Create our map
+    enumMap.insert(pair<string,soundElement> ("sCoin",sCoin));
+}
+
+//**********************************************************************//
+
+SoundCollection::~SoundCollection()
+{
+    //dtor
+}
+
+//**********************************************************************//
+
+Sound* SoundCollection::getSound(soundElement element){
+    return collection[element];
+}
+
+//**********************************************************************//
+
+Sound* SoundCollection::getSound(string element){
+    return collection[(enumMap.find(element))->second];
+}
---
+++ SDLGame-TFG/GraphicEngine/soundcollection.h
@@ -0,0 +1,62 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef SOUNDCOLLECTION_H
+#define SOUNDCOLLECTION_H
+
+#include "sound.h"
+#include <vector>
+#include <map>
+#include <string>
+
+enum soundElement{
+    sCoin //0
+};
+
+class SoundCollection
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~SoundCollection();
+
+        static SoundCollection * getInstance(){
+            if(instance == NULL)
+                instance = new SoundCollection();
+
+            return instance;
+        }
+
+        Sound * getSound(soundElement element);
+        Sound * getSound(string element);
+    protected:
+
+    private:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        SoundCollection();
+
+        static SoundCollection* instance;
+        vector<Sound *> collection;
+        map<string,soundElement> enumMap;
+};
+
+#endif // SOUNDCOLLECTION_H
