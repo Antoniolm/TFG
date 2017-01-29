0ab9c58d0df6fb88a5aaa3c91536a59f3a749bf9
Antoniolm <codiak19@gmail.com>
2017-01-29 09:49:35 +0100

    Added a new class -> weapon
    That class will manage the model/material of our currentWeapon and the
    damage that our/enemies can do .

---
+++ SDLGame-TFG/VideoGame/weapon.cpp
@@ -0,0 +1,58 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#include "weapon.h"
+
+Weapon::Weapon(vec3f aPos,WeaponType atype,float aDamage,MeshIndex mesh,MaterialIndex material)
+{
+    damage=aDamage;
+    position=vec4f(aPos.x,aPos.y,aPos.z,1.0);
+
+    MeshCollection * meshCollect= MeshCollection::getInstance();
+    MaterialCollection * materialCollect= MaterialCollection::getInstance();
+
+    Matrix4f * transMatrix=new Matrix4f();
+    transMatrix->translation(position.x,position.y,position.z);
+
+    root=new NodeSceneGraph();
+    root->add(transMatrix);
+    root->add(materialCollect->getMaterial(material));
+    root->add(meshCollect->getMesh(mesh));
+
+    currentTime=SDL_GetTicks();
+}
+
+//**********************************************************************//
+
+Weapon::~Weapon()
+{
+    //dtor
+}
+//**********************************************************************//
+
+void Weapon::visualization(Context & cv){
+    root->visualization(cv);
+}
+
+//**********************************************************************//
+
+void Weapon::updateState(float time,const Uint8* currentKeyStates,RootMap * rootMap ){
+
+}
+
