834a6f4577597578b70bcb8bd37ddfd667f4d182
Antoniolm <codiak19@gmail.com>
2017-01-30 21:25:40 +0100

    Added a new class -> controller <abstract>
    That system will manage the controller of our player (gamePad,keyboard)

---
+++ SDLGame-TFG/GraphicEngine/controller.cpp
@@ -0,0 +1,76 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#include "controller.h"
+
+Controller::Controller()
+{
+    //ctor
+}
+
+//**********************************************************************//
+
+Controller::~Controller()
+{
+    //dtor
+}
+
+//**********************************************************************//
+void Controller::setButtonUp(bool value){}
+bool Controller::checkButtonUp(){}
+
+//**********************************************************************//
+
+void Controller::setButtonDown(bool value){}
+bool Controller::checkButtonDown(){}
+
+//**********************************************************************//
+
+void Controller::setButtonLeft(bool value){}
+bool Controller::checkButtonLeft(){}
+
+//**********************************************************************//
+
+void Controller::setButtonRight(bool value){}
+bool Controller::checkButtonRight(){}
+
+//**********************************************************************//
+
+void Controller::setButtonInteract(bool value){}
+bool Controller::checkButtonInteract(){}
+
+//**********************************************************************//
+
+void Controller::setButtonJump(bool value){}
+bool Controller::checkButtonJump(){}
+
+//**********************************************************************//
+
+void Controller::setButtonAttack(bool value){}
+bool Controller::checkButtonAttack(){}
+
+//**********************************************************************//
+
+void Controller::setButtonShield(bool value){}
+bool Controller::checkButtonShield(){}
+
+//**********************************************************************//
+
+void Controller::setButtonSwap(bool value){}
+bool Controller::checkButtonSwap(){}
---
+++ SDLGame-TFG/GraphicEngine/controller.h
@@ -0,0 +1,68 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+
+class Controller
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        Controller();
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~Controller();
+
+        virtual void setButtonUp(bool value);
+        virtual bool checkButtonUp();
+
+        virtual void setButtonDown(bool value);
+        virtual bool checkButtonDown();
+
+        virtual void setButtonLeft(bool value);
+        virtual bool checkButtonLeft();
+
+        virtual void setButtonRight(bool value);
+        virtual bool checkButtonRight();
+
+        virtual void setButtonInteract(bool value);
+        virtual bool checkButtonInteract();
+
+        virtual void setButtonJump(bool value);
+        virtual bool checkButtonJump();
+
+        virtual void setButtonAttack(bool value);
+        virtual bool checkButtonAttack();
+
+        virtual void setButtonShield(bool value);
+        virtual bool checkButtonShield();
+
+        virtual void setButtonSwap(bool value);
+        virtual bool checkButtonSwap();
+    protected:
+
+    private:
+};
+
+#endif // CONTROLLER_H
