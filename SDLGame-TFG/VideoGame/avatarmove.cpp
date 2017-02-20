71c0b6832b19d6a751152f9a9be01347e698e16c
Antoniolm <codiak19@gmail.com>
2017-02-20 17:04:26 +0100

    Fixed a little bug in our gravity
    Sometimes our heroe was bad movement.For examples, the hero can appear
    on a wall  before a jump when the hero can't do that.

--- SDLGame-TFG/VideoGame/avatarmove.cpp
+++ SDLGame-TFG/VideoGame/avatarmove.cpp
@@ -257,7 +257,7 @@ ObjectScene * AvatarMove::gravity(float time){
     float tenthValueX,tenthValueZ;
 
     if(!isJumping){
-    GLfloat * moveGravity=acceleratedMove->updateState(time-currentTime).getMatrix();
+        GLfloat * moveGravity=acceleratedMove->updateState(time-currentTime).getMatrix();
 
     vec3f posHero=getPosition();
     posHero.y-=0.5;
@@ -309,7 +309,7 @@ ObjectScene * AvatarMove::gravity(float time){
         if(isFalling){
             vec3f positionObs=hasCollision->getPosition();
             BoundingBox box=hasCollision->getBoundingBox();
-            if((positionObs.y+box.maxValue.y)-(posHero.y)>0.1){
+            if((positionObs.y+box.maxValue.y)-(posHero.y)>0.1 && (positionObs.y+box.maxValue.y)-(posHero.y)<0.5){
                 Matrix4f trans;
                 trans.translation(0.0,(positionObs.y+box.maxValue.y)-(posHero.y),0.0);
                 moveAvatar->product(trans.getMatrix());
