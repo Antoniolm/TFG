37c4234af17c7de540e092cdd625459a616d4aa0
Antoniolm <codiak19@gmail.com>
2017-02-23 13:54:18 +0100

    Added a new class -> MovieScreen
    That class will manage the movie of our game. For example, when an user
    start the game that user will see a little movie/film about the history
    of the game

---
+++ SDLGame-TFG/GraphicEngine/moviescreen.cpp
@@ -0,0 +1,87 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#include "moviescreen.h"
+
+MovieScreen::MovieScreen()
+{
+    currentOption=0;
+    animationDelay=delay;
+    MeshCollection * meshCollect =MeshCollection::getInstance();
+
+    currentMaterial=new Material(vec3f(1.0,1.0,1.0),vec3f(1.0f, 0.5f, 0.5f),vec3f(0.5f, 0.5f, 0.5f),32.0f,"./textures/loading1.png");
+
+    Matrix4f * positionMenu=new Matrix4f();
+    positionMenu->translation(0.0,6.77,11.0);
+
+    Matrix4f * scaleMenu=new Matrix4f();
+    scaleMenu->scale(1.0,4.1,0.5);
+
+    Matrix4f * rotationMenu=new Matrix4f();
+    rotationMenu->rotation(20,1.0,0.0,0.0);
+
+    root=new NodeSceneGraph(false,true);
+    root->add(positionMenu);
+    root->add(rotationMenu);
+    root->add(scaleMenu);
+    root->add(currentMaterial);
+    root->add(meshCollect->getMesh(TEXT));
+
+    //Add the options
+    options.push_back(new Texture("./textures/loading1.png"));
+    options.push_back(new Texture("./textures/loading2.png"));
+    options.push_back(new Texture("./textures/loading3.png"));
+
+    currentTime=SDL_GetTicks();
+    loadDelay=currentTime;
+}
+
+//**********************************************************************//
+
+MovieScreen::~MovieScreen()
+{
+    //dtor
+}
+
+//**********************************************************************//
+
+void MovieScreen::visualization(Context & cv){
+    root->visualization(cv);
+}
+
+//**********************************************************************//
+
+void MovieScreen::updateState(GameState & gameState){
+    float time=gameState.time;
+
+    if(time-currentTime>200)
+        currentTime=time-50;
+
+    if(loadDelay<(time-animationDelay)){//if the time of our currentTexture is finished
+        currentOption++;                //Change texture
+        if((unsigned)currentOption==options.size())
+            currentOption=0;
+
+        currentMaterial->setTexture(options[currentOption]);
+        loadDelay=time;
+    }
+
+    currentTime+=time-currentTime;
+}
+
---
+++ SDLGame-TFG/GraphicEngine/moviescreen.h
@@ -0,0 +1,76 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef MOVIESCREEN_H
+#define MOVIESCREEN_H
+
+#include "object3d.h"
+#include "rootmap.h"
+#include "context.h"
+#include "mesh.h"
+#include "nodescenegraph.h"
+#include "texture.h"
+#include "material.h"
+#include "profile.h"
+#include "sound.h"
+#include "gamestate.h"
+#include "meshcollection.h"
+#include "materialcollection.h"
+
+class MovieScreen : public Object3D
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        MovieScreen();
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~MovieScreen();
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will show the object in our interface
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        void visualization(Context & cv);
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will update the state of the object. That change need the
+        *    current time in our application
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        void updateState(GameState & gameState);
+
+    protected:
+
+    private:
+        NodeSceneGraph * root;
+        vector<Texture *> options;
+        int currentOption;
+        Material * currentMaterial;
+        float animationDelay,loadDelay;
+};
+
+#endif // MOVIESCREEN_H
