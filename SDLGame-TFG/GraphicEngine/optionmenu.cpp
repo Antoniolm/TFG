42322f05cbbc6be3b5d7b6273185382b2ba2ccda
Antoniolm <codiak19@gmail.com>
2017-03-10 11:58:13 +0100

    Added a new class -> optionMenu
    That menu will manage the option of our game. The user will be able to
    change some feature of our game. That options are:
    Resolution,windowType, volume

---
+++ SDLGame-TFG/GraphicEngine/optionmenu.cpp
@@ -0,0 +1,155 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#include "optionmenu.h"
+
+OptionMenu::OptionMenu()
+{
+    currentOption=0;
+    activateMenu=true;
+    MeshCollection * meshCollect =MeshCollection::getInstance();
+    SoundCollection * soundCollect =SoundCollection::getInstance();
+
+    currentMaterial=new Material(vec3f(0.6f, 0.6f, 0.6f),vec3f(1.0f, 0.5f, 0.5f),vec3f(0.5f, 0.5f, 0.5f),32.0f,"./textures/mainMenuStart.png");
+    Material * materialBack=new Material(vec3f(1.0f, 1.0f, 1.0f),vec3f(1.0f, 0.5f, 0.5f),vec3f(0.5f, 0.5f, 0.5f),32.0f,"./textures/mainBackMenu.png");
+
+    positionMenu=new Matrix4f();
+    positionMenu->translation(0.0,6.752,11.0);
+
+    Matrix4f * betweenMenu=new Matrix4f();
+    betweenMenu->translation(0.0,0.0,-0.2);
+
+    Matrix4f * scaleMenu=new Matrix4f();
+    scaleMenu->scale(0.4,1.8,0.5);
+    Matrix4f * scaleMenuBack=new Matrix4f();
+    scaleMenuBack->scale(2.5,2.275,2.5);
+
+    Matrix4f * rotationMenu=new Matrix4f();
+    rotationMenu->rotation(20,1.0,0.0,0.0);
+
+    root=new NodeSceneGraph(false,true);
+    root->add(positionMenu);
+    root->add(rotationMenu);
+    root->add(scaleMenu);
+    root->add(currentMaterial);
+    root->add(meshCollect->getMesh(TEXT));
+    root->add(betweenMenu);
+    root->add(scaleMenuBack);
+    root->add(materialBack);
+    root->add(meshCollect->getMesh(TEXT));
+    currentTime=SDL_GetTicks();
+    menuDelay=currentTime;
+
+    openSound=soundCollect->getSound(sOpen);
+    moveSound=soundCollect->getSound(sCoin);
+
+    //Add the options
+    addOption(new Texture("./textures/mainMenuStart.png"));
+    addOption(new Texture("./textures/mainMenuCont.png"));
+    addOption(new Texture("./textures/mainMenuControl.png"));
+    addOption(new Texture("./textures/mainMenuQuit.png"));
+}
+
+//**********************************************************************//
+
+OptionMenu::~OptionMenu()
+{
+    delete root;
+
+    for(unsigned i=0;i<options.size();i++)
+        delete options[i];
+}
+
+//**********************************************************************//
+
+void OptionMenu::visualization(Context & cv){
+    if(activateMenu)
+        root->visualization(cv);
+}
+
+//**********************************************************************//
+
+void OptionMenu::updateState(GameState & gameState){
+    vec3f position;
+     string fileLoad;
+
+    float time=gameState.time;
+    ControllerManager * controller=gameState.controller;
+
+    if(time-currentTime>200)
+        currentTime=time-50;
+
+    if(activateMenu){ //If the menu is activated
+        if(controller->checkButton(cUP) && menuDelay<(time-300)){ //If the user push the action move on the menu
+            currentOption-=1;
+            if(currentOption==-1)
+                currentOption=(options.size()-1);
+
+            currentMaterial->setTexture(options[currentOption]);
+            menuDelay=time;
+            moveSound->play();
+        }
+        else if(controller->checkButton(cDOWN) && menuDelay<(time-300)){ //If the user push the action move on the menu
+            currentOption++;
+            if((unsigned)currentOption==options.size())
+                currentOption=0;
+
+            currentMaterial->setTexture(options[currentOption]);
+            menuDelay=time;
+            moveSound->play();
+        }
+        if(controller->checkButton(cACTION) && menuDelay<(time-300)){ //If the user push the action intro
+            SavedManager * saveManager;
+            switch(currentOption){
+                case 0: //Start Game
+
+                    openSound->play();
+                break;
+                case 1: //Continue
+                    openSound->play();
+                break;
+                case 2: //Controls
+                    openSound->play();
+                break;
+                case 3: //Exit
+                    exit(0);
+                break;
+
+            }
+
+            menuDelay=time;
+            controller->setState(false,cACTION);
+        }
+    }
+
+    currentTime+=time-currentTime;
+}
+
+//**********************************************************************//
+
+void OptionMenu::setPosition(vec3f aPosition){
+    positionMenu->translation(aPosition.x,aPosition.y+6.75,aPosition.z+11.0);
+}
+
+
+//**********************************************************************//
+
+void OptionMenu::activate(){
+    activateMenu=true;
+}
---
+++ SDLGame-TFG/GraphicEngine/optionmenu.h
@@ -0,0 +1,83 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2016-2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
+#ifndef OPTIONMENU_H
+#define OPTIONMENU_H
+
+#include "object3d.h"
+#include "rootmap.h"
+#include "context.h"
+#include "mesh.h"
+#include "nodescenegraph.h"
+#include "texture.h"
+#include "material.h"
+#include "sound.h"
+#include "menu.h"
+#include "profile.h"
+#include "mainmenu.h"
+#include "meshcollection.h"
+#include "materialcollection.h"
+#include "soundcollection.h"
+
+class OptionMenu : public Menu
+{
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        /** Constructor */
+        //////////////////////////////////////////////////////////////////////////
+        OptionMenu();
+
+        //////////////////////////////////////////////////////////////////////////
+        /** Destructor */
+        //////////////////////////////////////////////////////////////////////////
+        virtual ~OptionMenu();
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will show the object in our interface
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        virtual void visualization(Context & cv);
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will update the state of the object. That change need the
+        *    current time in our application
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        virtual void updateState(GameState & gameState);
+
+        void setPosition(vec3f aPosition);
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    The method will activate our menu
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
+        void activate();
+
+    protected:
+
+    private:
+        Sound * openSound,* moveSound;
+};
+
+#endif // OPTIONMENU_H
