aa93c9528540525f59b1997e9a930e646e25b815
Antoniolm <codiak19@gmail.com>
2017-03-30 22:52:35 +0200

    updated the speed of our arrow

--- SDLGame-TFG/VideoGame/avatarmove.cpp
+++ SDLGame-TFG/VideoGame/avatarmove.cpp
@@ -617,42 +617,42 @@ Projectile * AvatarMove::createProjectile(float damage){
     switch(direction){
         case FORWARD:
                 posProject=vec3f(position.x,position.y,position.z+0.5);
-                velocityProject=vec3f(0.0,0.0,2.0);
+                velocityProject=vec3f(0.0,0.0,4.0);
                 dirProject=FORWARD;
             break;
         case BACKWARD:
                 posProject=vec3f(position.x,position.y,position.z-0.5);
-                velocityProject=vec3f(0.0,0.0,-2.0);
+                velocityProject=vec3f(0.0,0.0,-4.0);
                 dirProject=BACKWARD;
             break;
         case LEFTWARD:
                 posProject=vec3f(position.x-0.5,position.y,position.z);
-                velocityProject=vec3f(-2.0,0.0,0.0);
+                velocityProject=vec3f(-4.0,0.0,0.0);
                 dirProject=LEFTWARD;
             break;
         case RIGHTWARD:
                 posProject=vec3f(position.x+0.5,position.y,position.z);
-                velocityProject=vec3f(2.0,0.0,0.0);
+                velocityProject=vec3f(4.0,0.0,0.0);
                 dirProject=RIGHTWARD;
             break;
         case FOR_LEFTWARD:
                 posProject=vec3f(position.x-0.5,position.y,position.z+0.5);
-                velocityProject=vec3f(-2.0,0.0,2.0);
+                velocityProject=vec3f(-4.0,0.0,4.0);
                 dirProject=FOR_LEFTWARD;
             break;
         case FOR_RIGHTWARD:
                 posProject=vec3f(position.x+0.5,position.y,position.z+0.5);
-                velocityProject=vec3f(2.0,0.0,2.0);
+                velocityProject=vec3f(4.0,0.0,4.0);
                 dirProject=FOR_RIGHTWARD;
             break;
         case BACK_LEFTWARD:
                 posProject=vec3f(position.x-0.5,position.y,position.z-0.5);
-                velocityProject=vec3f(-2.0,0.0,-2.0);
+                velocityProject=vec3f(-4.0,0.0,-4.0);
                 dirProject=BACK_LEFTWARD;
             break;
         case BACK_RIGHTWARD:
                 posProject=vec3f(position.x+0.5,position.y,position.z-0.5);
-                velocityProject=vec3f(2.0,0.0,-2.0);
+                velocityProject=vec3f(4.0,0.0,-4.0);
                 dirProject=BACK_RIGHTWARD;
 
             break;
